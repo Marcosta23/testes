896202cd2cce752a14bf1a5d0a80030df38a9eac
Marcosta23 <mpc2073@yahoo.com.br>
2022-02-18 13:25:33 -0300

    first

---
+++ arrays.js
@@ -0,0 +1,21 @@
+let array = ['string', 1, true, ['array1'], ['array2'], ['array3'], ['array4']];
+
+//console.log(array)
+//console.log(array[2]);
+
+// forEach
+
+/*array.forEach(function(item, index){
+    console.log(item, index)
+})*/
+
+/*array.push('marco')
+console.log(array)*/
+
+/*array.pop();
+console.log(array)*/
+
+        //  alert("Escolheu: " + botoes[i].value);
+        
+
+
